b843d34adaa1acb0465010bca49ea94a5de37540
Jimmy Wen <JimmyWen@Jimmys-MacBook.local>
2019-10-21 18:16:53 +0800

    Updates

---
+++ precache-manifest.33ac3f56e27394a74b28690b6b9bebad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611329bcb6a130f31f67511c51d24a0b",
+    "url": "/punk-beer/index.html"
+  },
+  {
+    "revision": "73abe93155809e0f85b5",
+    "url": "/punk-beer/static/css/main.ce00591c.chunk.css"
+  },
+  {
+    "revision": "faa37f82c77c2c367db9",
+    "url": "/punk-beer/static/js/2.1822bdb0.chunk.js"
+  },
+  {
+    "revision": "73abe93155809e0f85b5",
+    "url": "/punk-beer/static/js/main.6761db82.chunk.js"
+  },
+  {
+    "revision": "c4964d6a0833edb60ecd",
+    "url": "/punk-beer/static/js/runtime-main.c3e32c1a.js"
+  }
+]);
